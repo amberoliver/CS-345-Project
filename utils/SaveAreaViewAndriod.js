5387fb0588c3d36d4d7e1639de3f265bc27b0ec3
Martin Nester <martinnester@Martins-MacBook-Pro.local>
2022-03-29 01:32:00 -0400

    Added back background that I overrote in the last commit :)

--- utils/SaveAreaViewAndriod.js
+++ utils/SaveAreaViewAndriod.js
@@ -3,7 +3,6 @@ import { StyleSheet, Platform, StatusBar } from "react-native";
 export default StyleSheet.create({
   AndroidSafeArea: {
     flex: 1,
-    backgroundColor: "white",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
