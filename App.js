51d9ffbd667ad7006b40a59712298f34554ee639
Martin Nester <martinn7352@gmail.com>
2022-03-29 15:40:25 -0400

    added the bottom sheet modal to be used for when adding transactions

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // In App.js in a new project
-
+import "react-native-gesture-handler";
 import * as React from "react";
+import { useRef, useMemo } from "react";
 import { View, Text, Pressable } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -11,80 +12,108 @@ import TransactionEdit from "./pages/TransactionEdit";
 import Transactions from "./pages/Transactions";
 import { Feather } from "@expo/vector-icons";
 import Settings from "./pages/Settings";
+import BottomSheet, { BottomSheetBackdrop } from "@gorhom/bottom-sheet";
 
 const Tab = createBottomTabNavigator();
 function Tabs() {
+  // ref
+  const bottomSheetRef = useRef(null);
+  // variables
+  const snapPoints = useMemo(() => ["50%", "80%"], []);
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: "#6B7AFF",
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarIcon: (props) => <Feather name="home" {...props} />,
+    <>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#6B7AFF",
         }}
-      />
-      <Tab.Screen
-        name="Categories"
-        component={Categories}
-        options={{
-          tabBarIcon: (props) => <Feather name="pie-chart" {...props} />,
-        }}
-      />
-      <Tab.Screen
-        name="Add"
-        component={TransactionEdit}
-        options={{
-          tabBarButton: (props) => (
-            <Pressable
-              {...props}
-              style={[
-                props.style,
-                {
-                  justifyContent: "center",
-                  alignItems: "center",
-                },
-              ]}
-            >
-              <View
-                style={{
-                  shadowColor: "black",
-                  shadowOpacity: 0.1,
-                  shadowRadius: 2,
-                  shadowOffset: { width: 2, height: 2 },
-                  height: "80%",
-                  aspectRatio: 1,
-                  backgroundColor: "#6B7AFF",
-                  justifyContent: "center",
-                  borderRadius: 1000,
-                  alignItems: "center",
-                }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: (props) => <Feather name="home" {...props} />,
+          }}
+        />
+        <Tab.Screen
+          name="Categories"
+          component={Categories}
+          options={{
+            tabBarIcon: (props) => <Feather name="pie-chart" {...props} />,
+          }}
+        />
+        <Tab.Screen
+          name="Add"
+          component={TransactionEdit}
+          options={{
+            tabBarButton: (props) => (
+              <Pressable
+                {...props}
+                style={[
+                  props.style,
+                  {
+                    justifyContent: "center",
+                    alignItems: "center",
+                  },
+                ]}
               >
-                <Feather name="plus" size={28} color="white" />
-              </View>
-            </Pressable>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Transactions"
-        component={Transactions}
-        options={{
-          tabBarIcon: (props) => <Feather name="file-text" {...props} />,
-        }}
-      />
-      <Tab.Screen
-        name="Settings"
-        component={Settings}
-        options={{
-          tabBarIcon: (props) => <Feather name="settings" {...props} />,
-        }}
-      />
-    </Tab.Navigator>
+                <View
+                  style={{
+                    shadowColor: "black",
+                    shadowOpacity: 0.1,
+                    shadowRadius: 2,
+                    shadowOffset: { width: 2, height: 2 },
+                    height: "80%",
+                    aspectRatio: 1,
+                    backgroundColor: "#6B7AFF",
+                    justifyContent: "center",
+                    borderRadius: 1000,
+                    alignItems: "center",
+                  }}
+                >
+                  <Feather name="plus" size={28} color="white" />
+                </View>
+              </Pressable>
+            ),
+          }}
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault();
+              bottomSheetRef.current.snapToIndex(0);
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Transactions"
+          component={Transactions}
+          options={{
+            tabBarIcon: (props) => <Feather name="file-text" {...props} />,
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarIcon: (props) => <Feather name="settings" {...props} />,
+          }}
+        />
+      </Tab.Navigator>
+      <BottomSheet
+        snapPoints={snapPoints}
+        ref={bottomSheetRef}
+        index={-1}
+        enablePanDownToClose
+        backdropComponent={(props) => (
+          <BottomSheetBackdrop
+            appearsOnIndex={0}
+            disappearsOnIndex={-1}
+            {...props}
+          />
+        )}
+      >
+        <TransactionEdit />
+      </BottomSheet>
+    </>
   );
 }
 
