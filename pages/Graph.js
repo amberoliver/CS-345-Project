1bb207b7ce23f1d9e8d3f0bf6c956bb818428ae6
Amber Oliver <amb@Ambers-MacBook-Pro.local>
2022-03-30 14:23:03 -0400

    changes

--- pages/Graph.js
+++ pages/Graph.js
@@ -1,9 +1,72 @@
 import { View } from "react-native";
+import { PieChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+
+const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+
+  const screenWidth = Dimensions.get("window").width;
+
+const data = [
+    {
+      name: "Seoul",
+      budget: 21500000,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Toronto",
+      budget: 2800000,
+      color: "#F00",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Beijing",
+      budget: 527612,
+      color: "red",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "New York",
+      budget: 8538000,
+      color: "#ffffff",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Moscow",
+      budget: 11920000,
+      color: "rgb(0, 0, 255)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    }
+  ];
 
 export default function Graph() {
     return (
 <View>
-    
+    <PieChart
+  data={data}
+  width={screenWidth}
+  height={200}
+  chartConfig={chartConfig}
+  accessor={"budget"}
+  backgroundColor={"transparent"}
+  paddingLeft={"15"}
+  center={[10, 10]}
+  absolute
+/>
 </View>
     );
 }
