c14a4f57a7381d5c48616914174c23314c4cadb2
Amber Oliver <amb@Ambers-MacBook-Pro.local>
2022-03-30 12:16:45 -0400

    changes

--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,7 @@
-import { View, Text } from "react-native";
 import Chart from "../components/Chart";
+import Categories from "./Categories";
+import { ScrollView } from "react-native-gesture-handler";
+
 const mockCategories = [
   { name: "Food", remaining: 200, total: 300, color: "#6B7AFF" },
   { name: "Rent", remaining: 400, total: 400, color: "#FF6BF0" },
@@ -8,8 +10,9 @@ const mockCategories = [
 ];
 export default function Home() {
   return (
-    <View>
+    <ScrollView>
       <Chart categories={mockCategories} />
-    </View>
+      <Categories></Categories>
+    </ScrollView>
   );
 }
