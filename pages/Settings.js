deaf1c723ffeb6369458595ede6657a7ec337568
Amber Oliver <amb@Ambers-MacBook-Pro.local>
2022-03-29 22:00:20 -0400

    changes

--- pages/Settings.js
+++ pages/Settings.js
@@ -3,27 +3,38 @@ import Card from "../components/Card";
 import Transaction from "../components/Transaction";
 import Button from "../components/Button";
 
+const nameTitle = "Name:";
+const emailTitle = "Email:";
+const phoneTitle = "Phone:";
+
 const mockSettings = [
   {
-    name: "Name",
-    
-  },
-  {
-    name: "Email",
-
-  },
-  {
-    name: "Phone",
-
+    name: "Amber",
+    email: "amberoliver@gmail.com",
+    phone: "808.681.9113",
   },
 ];
-export default function Settings() {
+
+export default function Settings(name, email, phone) {
   return (
     <View>
-      <Card margin noPad>
-        {mockSettings.map((data, index) => (
-          <Transaction {...data} key={index} />
-        ))}
+      <Card>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>{nameTitle}</Text>
+      <Text style={{ fontSize: 25, flexDirection: "row", textAlign: "left" }}>
+            {name}
+          </Text>
+      </Card>
+      <Card>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>{emailTitle}</Text>
+      <Text style={{ fontSize: 25, flexDirection: "row", textAlign: "left" }}>
+            {email}
+          </Text>
+      </Card>
+      <Card>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>{phoneTitle}</Text>
+      <Text style={{ fontSize: 25, flexDirection: "row", textAlign: "left" }}>
+            {phone}
+          </Text>
       </Card>
       <Button title="Get Help" onPress={() => console.log("button pressed")} />
       <Button title="Logout" onPress={() => console.log("button pressed")} />
