da787be836e97facfdad97b68c9ec901cfd9d62a
Martin Nester <martinn7352@gmail.com>
2022-04-11 17:35:19 -0400

    validate settings page

--- pages/Settings.js
+++ pages/Settings.js
@@ -3,20 +3,41 @@ import Card from "../components/Card";
 import Transaction from "../components/Transaction";
 import Button from "../components/Button";
 import useColor from "../useColor";
+import { useEffect, useState } from "react";
 
-const nameTitle = "Name:";
-const emailTitle = "Email:";
-const phoneTitle = "Phone:";
+import { validate as isValidEmail } from "email-validator";
 
 export default function Settings() {
-  const name = "Amber";
-  const email = "amberoliver@gmail.com";
-  const phone = "(252) 355-9323";
+  function validateEmail(email) {
+    if (!isValidEmail(email)) {
+      return "Email entered is not valid."; //return string with message if error
+    } else {
+      return null; //return null if no error
+    }
+  }
+
+  function validatePhone(phone) {
+    //phone will be a string
+    //implement this
+    return null;
+  }
+
   return (
     <View>
       <Setting title="Name:" placeholder="John Doe" />
-      <Setting title="Email:" placeholder="johndoe@gmail.com" />
-      <Setting title="Phone Number:" placeholder="(200) 425-4901" />
+      <Setting
+        title="Email:"
+        placeholder="johndoe@gmail.com"
+        validate={validateEmail}
+        autoCapitalize={"none"}
+        keyboardType="email-address"
+      />
+      <Setting
+        title="Phone Number:"
+        placeholder="2004254901"
+        validate={validatePhone}
+        keyboardType="number-pad"
+      />
       <Button title="Get Help" onPress={() => console.log("button pressed")} />
       <Button title="Logout" onPress={() => console.log("button pressed")} />
       <Button
@@ -27,23 +48,46 @@ export default function Settings() {
   );
 }
 
-function Setting({ title, value, placeholder }) {
+function Setting({ title, placeholder, validate, ...rest }) {
   const color = useColor();
+  const [prev, setPrev] = useState("");
+  const [value, setValue] = useState("");
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (validate) {
+      setError(validate(value) || null);
+    } else {
+      setPrev(value);
+    }
+  }, [value]);
+
+  function handleBlur() {
+    if (error) {
+      setValue(prev);
+      setError(null);
+    }
+  }
   return (
     <Card>
       <Text style={{ fontSize: 20, fontWeight: "bold", color: color.font }}>
         {title}
       </Text>
       <TextInput
+        {...rest}
         style={{
           fontSize: 25,
           flexDirection: "row",
           textAlign: "left",
           color: color.font,
         }}
-        defaultValue={value}
+        onBlur={handleBlur}
+        value={value}
+        onChangeText={setValue}
         placeholder={placeholder}
       ></TextInput>
+      <Text style={{ color: "red", display: error == null ? "none" : "flex" }}>
+        {error}
+      </Text>
     </Card>
   );
 }
